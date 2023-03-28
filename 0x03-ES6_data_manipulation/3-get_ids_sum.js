257b1b9483ed470ffef35265ebe2e9566cd24b3b
joyakinyi <joyakinyi940@gmail.com>
2023-03-28 16:56:41 +0000

    Data manipulation

--- 0x03-ES6_data_manipulation/3-get_ids_sum.js
+++ 0x03-ES6_data_manipulation/3-get_ids_sum.js
@@ -1,4 +1,9 @@
-function getStudentIdsSum(students) {
-  return students.reduce((acc, student) => acc + student.id, 0);
+export default function getStudentIdsSum(students) {
+  if (students instanceof Array) {
+    return students.reduce(
+      (prevStudent, curStudent) => prevStudent.id || prevStudent + curStudent.id,
+      0,
+    );
+  }
+  return 0;
 }
-
